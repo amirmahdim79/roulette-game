5513ad3ac0cbf432717d856deb78f0a902e83901
amir mahdi <mohamadian.am79@ut.ac.ir>
2024-01-23 20:05:25 +0330

    fix

--- index.js
+++ index.js
@@ -321,7 +321,9 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3003;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+io.listen(3003)
+
+// const PORT = process.env.PORT || 3003;
+// server.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
